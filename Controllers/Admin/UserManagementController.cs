ba8b6f04670ad0e89dbff53d02ac7f300fa273c4
Nic <3xpect1916@gmail.com>
2025-01-24 14:02:04 +0800

    added api to save edit to database

--- Controllers/Admin/UserManagementController.cs
+++ Controllers/Admin/UserManagementController.cs
@@ -16,5 +16,36 @@ namespace Backend.Controllers
             var rewardItems = await _context.Users.ToListAsync();
             return Ok(rewardItems);
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(string id, [FromBody] User updatedUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingUser = await _context.Users.FindAsync(id);
+            if (existingUser == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            // Update only allowed fields
+            existingUser.Name = updatedUser.Name;
+            existingUser.Email = updatedUser.Email;
+            existingUser.ContactNumber = updatedUser.ContactNumber;
+            existingUser.UserRole = updatedUser.UserRole;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(existingUser);
+            }
+            catch
+            {
+                return StatusCode(500, "An error occurred while updating the user.");
+            }
+        }
     }
 }
